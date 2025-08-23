4200d79c224d2e47cfb522774fae7a80d6e7a0f0
JulyKikuAkita <b92701105@gmail.com>
2025-08-22 22:46:12 -0700

    add drag drop gesture

--- macOSDemos/ContentView.swift
+++ macOSDemos/ContentView.swift
@@ -5,13 +5,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            if #available(macOS 26.0, *) {
+                ImageContainerView()
+                    .navigationTitle("Untitled Folder")
+                    .toolbar {
+                        ToolbarItem(placement: .primaryAction) {
+                            Button("Search", systemImage: "magnifyingglass") {
+    
+                            }
+                        }
+                        ToolbarItem(placement: .navigation) {
+                            HStack(spacing: 0) {
+                                Button("Back", systemImage: "chevron.left") {
+    
+                                }
+    
+                                Button(
+                                    "Forward",
+                                    systemImage: "chevron.right"
+                                ) {
+    
+                                }
+                            }
+                        }
+                    }
+            } else {
+                Text("Hello World")
+            }
         }
-        .padding()
     }
 }
 
